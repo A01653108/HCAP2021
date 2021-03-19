8dc9d39a1f57b4620761fb5f93e0ecfb3d14a7a0
Paola Dorantes <paoladorantes@MacBook-Pro-de-Paola.local>
2021-03-19 08:32:13 -0600

    nuevos cambios

--- cargaImagenes.py
+++ cargaImagenes.py
@@ -1,9 +1,10 @@
 import cv2
 import numpy as np
-IRGB =cv2.imread("Spiderman.jpg")
+IRGB = cv2.imread("spiderman.jpg")
 print(IRGB)
 print(IRGB.shape)
 IGS=cv2.cvtColor(IRGB,cv2.COLOR_BGR2GRAY)
 print(IGS)
 print(IGS.shape)
+cv2.imwrite("spiderman.jpg,IGS)
 
