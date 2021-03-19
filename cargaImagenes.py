5e5fe07f7e4fcf71d87b0325b1d1e2ed1ffac964
Paola Dorantes <paoladorantes@MacBook-Pro-de-Paola.local>
2021-03-19 09:28:40 -0600

    procesar imagenes

--- cargaImagenes.py
+++ cargaImagenes.py
@@ -1,10 +1,11 @@
 import cv2
 import numpy as np
-IRGB = cv2.imread("spiderman.jpg")
+IRGB = cv2.imread("Spiderman.jpg")
 print(IRGB)
 print(IRGB.shape)
+print("Lineas agrgadas en la rama2")
 IGS=cv2.cvtColor(IRGB,cv2.COLOR_BGR2GRAY)
 print(IGS)
 print(IGS.shape)
-cv2.imwrite("spiderman.jpg,IGS)
+cv2.imwrite("Spiderman.jpg,IGS)
 
--- convolucion.py
+++ convolucion.py
@@ -27,7 +27,7 @@ I=[[2,0,1,1,1],[3,0,0,0,2],[1,1,1,1,1],[3,1,1,1,2],[1,1,1,1,1]]
 In=np.array(I)
 Kn=np.array(K)
 
-IRGB=cv2.imread('004.jpg')
+IRGB=cv2.imread('Spiderman.jpg')
 IGS=cv2.cvtColor(IRGB,cv2.COLOR_BGR2GRAY)
 print(IGS.shape)
 
